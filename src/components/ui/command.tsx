2a7c2b62482081db8da6c6255a592976f7a52c89
md-jahid-islam <jahidulislamwebbd@gmail.com>
2025-07-09 21:07:39 +0600

    added to sheet page

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -25,7 +25,6 @@ import { Dialog, DialogContent } from "@/components/ui/dialog"
     </Dialog>
   )
  }
-
  const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>>
@@ -33,7 +32,7 @@ import { Dialog, DialogContent } from "@/components/ui/dialog"
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
     <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
-      ref={ref} className={cn( "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50", className )} {...props}/>
+    ref={ref} className={cn( "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50", className )} {...props}/>
   </div>
  ))
 
@@ -88,7 +87,6 @@ import { Dialog, DialogContent } from "@/components/ui/dialog"
   )
  }
  CommandShortcut.displayName = "CommandShortcut"
-
  export {
   Command,
   CommandDialog,
